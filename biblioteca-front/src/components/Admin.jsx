c587a54520610ca2d66828dfe9b4bd28d68b3226
Matias <matias.ds.soto@gmail.com>
2024-12-13 09:13:29 -0300

    inclusion de alerta de cerrar sesion

--- biblioteca-front/src/components/Admin.jsx
+++ biblioteca-front/src/components/Admin.jsx
@@ -3,11 +3,12 @@ import { useNavigate } from "react-router-dom";
 import { useLibrary } from "../context/LibraryContext";
 import Libro from "./Libro";
 import { Table, Container, Row, Col, Button, Form } from "react-bootstrap";
+import Swal from "sweetalert2";
 import './Admin.css';
 
 const Admin = () => {
   const { libros, estados, editoriales, idiomas, error } = useLibrary();
-  const navigate = useNavigate(); // Corregido aquí
+  const navigate = useNavigate(); 
 
   // Estados para los filtros de búsqueda
   const [searchTitle, setSearchTitle] = useState("");
@@ -50,142 +51,150 @@ const Admin = () => {
   const isSearchActive =
     searchTitle || searchAutor || searchEditorial || searchIdioma;
 
-    return (
-        <Container fluid className="text-center">
-          <Row>
-            <Col>
-              <h2 className="pb-3">Gestión de Libros</h2>
-              {error && <div className="alert alert-danger">{error}</div>}
-    
-              {/* Barra de Búsqueda */}
-              <Form className="mb-4">
-                <Row>
-                  <Col md={3} sm={12} className="mb-2">
-                    <Form.Control
-                      type="text"
-                      placeholder="Buscar por título"
-                      value={searchTitle}
-                      onChange={(e) => setSearchTitle(e.target.value)}
-                    />
-                  </Col>
-                  <Col md={3} sm={12} className="mb-2">
-                    <Form.Control
-                      type="text"
-                      placeholder="Buscar por autor"
-                      value={searchAutor}
-                      onChange={(e) => setSearchAutor(e.target.value)}
-                    />
-                  </Col>
-                  <Col md={3} sm={12} className="mb-2">
-                    <Form.Control
-                      type="text"
-                      placeholder="Buscar por editorial"
-                      value={searchEditorial}
-                      onChange={(e) => setSearchEditorial(e.target.value)}
-                    />
-                  </Col>
-                  <Col md={3} sm={12} className="mb-2">
-                    <Form.Control
-                      type="text"
-                      placeholder="Buscar por idioma"
-                      value={searchIdioma}
-                      onChange={(e) => setSearchIdioma(e.target.value)}
-                    />
-                  </Col>
-                </Row>
-              </Form>
-    
-              {/* Tabla de Libros */}
-              {isSearchActive && (
-                <div style={{ maxHeight: "400px", overflowY: "auto", scrollbarWidth: 'none',}}>
-                  <Table>
-                    <thead>
-                      <tr>
-                        <th className="table-dark">Título</th>
-                        <th className="table-dark">Autor</th>
-                        <th className="table-dark">Estado</th>
-                        <th className="table-dark">Editorial</th>
-                        <th className="table-dark">Idioma</th>
-                        <th className="table-dark">N° Copia</th>
-                        <th className="table-dark">Prestado</th>
-                        <th className="table-dark">Acciones</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      {libros.length === 0 ? (
-                        <tr>
-                          <td colSpan="8" className="bg-white text-center">
-                            No se encontraron libros disponibles.
+  return (
+    <Container fluid className="text-center">
+      <Row>
+        <Col>
+          <h2 className="pb-3">Gestión de Libros</h2>
+          {error && <div className="alert alert-danger">{error}</div>}
+
+          {/* Barra de Búsqueda */}
+          <Form className="mb-4">
+            <Row>
+              <Col md={3} sm={12} className="mb-2">
+                <Form.Control
+                  type="text"
+                  placeholder="Buscar por título"
+                  value={searchTitle}
+                  onChange={(e) => setSearchTitle(e.target.value)}
+                />
+              </Col>
+              <Col md={3} sm={12} className="mb-2">
+                <Form.Control
+                  type="text"
+                  placeholder="Buscar por autor"
+                  value={searchAutor}
+                  onChange={(e) => setSearchAutor(e.target.value)}
+                />
+              </Col>
+              <Col md={3} sm={12} className="mb-2">
+                <Form.Control
+                  type="text"
+                  placeholder="Buscar por editorial"
+                  value={searchEditorial}
+                  onChange={(e) => setSearchEditorial(e.target.value)}
+                />
+              </Col>
+              <Col md={3} sm={12} className="mb-2">
+                <Form.Control
+                  type="text"
+                  placeholder="Buscar por idioma"
+                  value={searchIdioma}
+                  onChange={(e) => setSearchIdioma(e.target.value)}
+                />
+              </Col>
+            </Row>
+          </Form>
+
+          {/* Tabla de Libros */}
+          {isSearchActive && (
+            <div style={{ maxHeight: "400px", overflowY: "auto", scrollbarWidth: 'none',}}>
+              <Table>
+                <thead>
+                  <tr>
+                    <th className="table-dark">Título</th>
+                    <th className="table-dark">Autor</th>
+                    <th className="table-dark">Estado</th>
+                    <th className="table-dark">Editorial</th>
+                    <th className="table-dark">Idioma</th>
+                    <th className="table-dark">N° Copia</th>
+                    <th className="table-dark">Prestado</th>
+                    <th className="table-dark">Acciones</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {libros.length === 0 ? (
+                    <tr>
+                      <td colSpan="8" className="bg-white text-center">
+                        No se encontraron libros disponibles.
+                      </td>
+                    </tr>
+                  ) : (
+                    filteredBooks.map((libro) => (
+                      <React.Fragment key={libro.id}>
+                        <Libro
+                          libro={libro}
+                          estados={estados}
+                          editoriales={editoriales}
+                          idiomas={idiomas}
+                        />
+                        <tr className="table-dark">
+                          <td colSpan="8">
+                            Total de libros disponibles para ser prestados:{" "}
+                            {libro.total_disponibles}
                           </td>
                         </tr>
-                      ) : (
-                        filteredBooks.map((libro) => (
-                          <React.Fragment key={libro.id}>
-                            <Libro
-                              libro={libro}
-                              estados={estados}
-                              editoriales={editoriales}
-                              idiomas={idiomas}
-                            />
-                            <tr className="table-dark">
-                              <td colSpan="8">
-                                Total de libros disponibles para ser prestados:{" "}
-                                {libro.total_disponibles}
-                              </td>
-                            </tr>
-                          </React.Fragment>
-                        ))
-                      )}
-                    </tbody>
-                  </Table>
-                </div>
-              )}
-    
-              {/* Botones de acción */}
-              <div className="text-center mt-4">
-                <Button
-                  variant="primary"
-                  onClick={() => navigate("/devolucion")}
-                  className="mx-auto d-block"
-                  size="sm"
-                >
-                  Registrar Devoluciones
-                </Button>
-              </div>
-              <div className="text-center mt-2">
-                <Button
-                  variant="primary"
-                  onClick={() => navigate("/nuevo-libro")}
-                  className="mx-auto d-block"
-                  size="sm"
-                >
-                  Registrar Nuevo Libro
-                </Button>
-              </div>
-              <div className="text-center mt-2">
-                <Button
-                  variant="danger"
-                  onClick={() => navigate("/eliminar-copia")}
-                  className="mx-auto d-block"
-                  size="sm"
-                >
-                  Borra Copia de un Libro
-                </Button>
-              </div>
-              <div className="text-center mt-2">
-                <Button
-                  variant="link"
-                  onClick={() => navigate("/")}
-                  className="text-decoration-none fw-bold text-light"
-                >
-                  Cerrar Sesión
-                </Button>
-              </div>
-            </Col>
-          </Row>
-        </Container>
-    );    
+                      </React.Fragment>
+                    ))
+                  )}
+                </tbody>
+              </Table>
+            </div>
+          )}
+
+          {/* Botones de acción */}
+          <div className="text-center mt-4">
+            <Button
+              variant="primary"
+              onClick={() => navigate("/devolucion")}
+              className="mx-auto d-block"
+              size="sm"
+            >
+              Registrar Devoluciones
+            </Button>
+          </div>
+          <div className="text-center mt-2">
+            <Button
+              variant="primary"
+              onClick={() => navigate("/nuevo-libro")}
+              className="mx-auto d-block"
+              size="sm"
+            >
+              Registrar Nuevo Libro
+            </Button>
+          </div>
+          <div className="text-center mt-2">
+            <Button
+              variant="danger"
+              onClick={() => navigate("/eliminar-copia")}
+              className="mx-auto d-block"
+              size="sm"
+            >
+              Borra Copia de un Libro
+            </Button>
+          </div>
+          <div className="text-center mt-2">
+            <Button
+              variant="link"
+              onClick={() => {
+                Swal.fire({
+                  icon: "success",
+                  title: "Sesión cerrada",
+                  text: "Usted ha cerrado sesión correctamente.",
+                  confirmButtonText: "Aceptar",
+                }).then(() => {
+                  navigate("/"); // Redirige después de que el usuario confirme
+                });
+              }}
+              className="text-decoration-none fw-bold text-light"
+            >
+              Cerrar Sesión
+            </Button>
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default Admin;
-
