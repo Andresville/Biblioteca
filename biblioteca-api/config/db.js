3e85096eadb62b6c5cc193b688005289c3517202
Matias <matias.ds.soto@gmail.com>
2024-12-13 09:39:30 -0300

    modificacion login db

--- biblioteca-api/config/db.js
+++ biblioteca-api/config/db.js
@@ -4,8 +4,8 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',  // Cambia según tu configuración
-    password: 'Emilia15#',  // Cambia según tu configuración
-    database: 'library_db'  // Nombre de tu base de datos
+    password: 'root',  // Cambia según tu configuración
+    database: 'biblioteca'  // Nombre de tu base de datos
 });
 
 connection.connect((err) => {
